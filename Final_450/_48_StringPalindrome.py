258ca086cf298859dc95f66ee84336017e96f5e3
shu3102 <shubhams3102@gmail.com>
2021-12-15 16:01:42 +0530

    14/12 Commit

---
+++ Final_450/_48_StringPalindrome.py
@@ -0,0 +1,12 @@
+
+def isPalindrome(s):
+    i = 0
+    j = len(s) - 1
+    while(i < j):
+        if(s[i] != s[j]):
+            return False
+        i += 1
+        j -= 1
+    return True
+
+print(isPalindrome("ShubhamahbuhS"))
